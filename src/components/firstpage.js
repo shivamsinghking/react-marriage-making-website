929878b92ce37c14d0fc2e15fd40bb6a885b73f1
shivamsinghking <singhsanajay20001@gmail.com>
2020-06-01 21:09:16 +0530

    initial commit

---
+++ src/components/firstpage.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import Images from './images'
+
+
+ class firstpage extends Component {
+    render(){
+        return (
+            <div>
+             <Images />
+            </div>
+          
+        
+        )
+    }
+}
+
+export default firstpage;
